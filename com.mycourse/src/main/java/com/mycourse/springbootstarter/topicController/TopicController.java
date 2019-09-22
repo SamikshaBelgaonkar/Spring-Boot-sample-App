02c4690e106c156d7351a51d113e87c49e16d72a
TechShizuka <samikshab14@gmail.com>
2019-09-23 00:29:20 +0530

    added controller class with GET,POST,PUT,DELETE endpoints

--- com.mycourse/src/main/java/com/mycourse/springbootstarter/topicController/TopicController.java
+++ com.mycourse/src/main/java/com/mycourse/springbootstarter/topicController/TopicController.java
@@ -1,15 +1,48 @@
 package com.mycourse.springbootstarter.topicController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycourse.springbootstarter.model.Topic;
+import com.mycourse.springbootstarter.topicService.TopicService;
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
-	public String getAllTopics()
+	public List<Topic> getAllTopics()
 	{
-		System.out.println("HIIIIII");
-		return "All Topics";
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void addTopic(@RequestBody Topic topic,@PathVariable String id)
+	{
+		topicService.updateTopic(id,topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id)
+	{
+		topicService.deleteTopic(id);
 	}
 }
